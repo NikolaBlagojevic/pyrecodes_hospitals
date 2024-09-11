2601f0e078bb806606c639cbf66ce76813f41ce9
Nikola Blagojević <blagojevic@ibk.baug.ethz.ch>
2024-09-11 13:22:23 +0200

    consumables consumed once at the start of patient's stay, consequences method modified to ensure worst consequence is considered

--- pyrecodes_hospitals/Patient.py
+++ pyrecodes_hospitals/Patient.py
@@ -7,6 +7,10 @@ class PatientType():
 
     EXIT = 'EXIT'
     STRETCHER_NAME = 'Stretcher'
+    ONE_TIME_CONSUMABLES = ['Stretcher', 'MCI_Kit_NonWalking_EmergencyDepartment', 
+                            'MCI_Kit_NonWalking_OperatingTheater', 'MCI_Kit_NonWalking_HighDependencyUnit',
+                            'MCI_Kit_NonWalking_Medical/SurgicalDepartment', 'MCI_Kit_NonWalking_RestOfHospital',
+                            'MCI_Kit_Walking_EmergencyDepartment', 'Blood']
 
     def set_parameters(self, patient_type_name, patient_type_parameters: list) -> None:
         self.name = patient_type_name
@@ -18,6 +22,7 @@ class PatientType():
         self.treated = False
         self.alive = True
         self.unmet_demand_info = {}
+        self.mortality_rate_record = []
         self.set_initial_department()     
     
     def set_initial_department(self) -> None:
@@ -53,16 +58,19 @@ class PatientType():
     
     def get_resource_demand(self) -> dict:
         if not(self.out_of_hospital()):
-            self.update_stretcher_demand()
+            self.update_consumable_demand()
             return self.demand[self.get_current_department_id()]
         else:
             return {}
     
-    def update_stretcher_demand(self) -> None:
+    def update_consumable_demand(self) -> None:
         # Stretchers are only needed in the first time step of the patient's stay at the department.
-        # If the patient is already at the department, then the patient has already been transferred to a bed and does not a stretcher.
+        # Other consumables (MCI kits, blood) are also only consumed once.
+        # If the patient is already at the department, then the patient has already been transferred to a bed and does not a stretcher.        
         if self.get_current_length_of_stay() > 0:
-            self.demand[self.get_current_department_id()][self.STRETCHER_NAME] = 0.0
+            for resource_name in self.demand[self.get_current_department_id()].keys():
+                if resource_name in self.ONE_TIME_CONSUMABLES:
+                    self.demand[self.get_current_department_id()][resource_name] = 0.0
     
     def get_current_department(self) -> str:
         return self.flow[-1]['Department']
@@ -85,6 +93,7 @@ class PatientType():
                 # set all demand to met so that during resource distribution only unmet demand is set, as it is implemented now
                 self.set_all_demand_as_met()
             self.check_if_alive()
+            self.record_mortality_rate()
             if self.stay_finished():
                 self.move_to_next_department()
          
@@ -105,14 +114,18 @@ class PatientType():
         for consequence in consequences:
             consequence_name, consequence_value = list(consequence.items())[0]
             if consequence_name == 'Mortality Rate Increase [per missing nurse]':
-                self.mortality_rate = self.update_patient_when_nurses_missing(resource_name, consequence_value, demand_met, self.mortality_rates[self.get_current_department_id()])
+                updated_mortality_rate = self.update_patient_when_nurses_missing(resource_name, consequence_value, demand_met, self.mortality_rates[self.get_current_department_id()])
+                self.mortality_rate = max(updated_mortality_rate, self.mortality_rate)
             elif consequence_name == 'Length Of Stay Extended [per missing nurse]':
-                self.length_of_stay = self.update_patient_when_nurses_missing(resource_name, consequence_value, demand_met, self.lengths_of_stay[self.get_current_department_id()])            
+                updated_length_of_stay = self.update_patient_when_nurses_missing(resource_name, consequence_value, demand_met, self.lengths_of_stay[self.get_current_department_id()])
+                self.length_of_stay = max(self.length_of_stay, updated_length_of_stay)
             elif consequence_name == 'Mortality Rate Increase':
-                self.mortality_rate = consequence_value * self.mortality_rates[self.get_current_department_id()]
+                updated_mortality_rate = consequence_value * self.mortality_rates[self.get_current_department_id()]
+                self.mortality_rate = max(updated_mortality_rate, self.mortality_rate)
                 self.patient_not_treated()
             elif consequence_name == 'Length Of Stay Extended':
-                self.length_of_stay = consequence_value * self.lengths_of_stay[self.get_current_department_id()]
+                updated_length_of_stay = consequence_value * self.lengths_of_stay[self.get_current_department_id()]
+                self.length_of_stay = max(self.length_of_stay, updated_length_of_stay)
                 self.patient_not_treated()
             elif consequence_name == 'Death In [hours]':
                 self.patient_not_treated()
@@ -143,8 +156,14 @@ class PatientType():
         return count
     
     def update_patient_when_nurses_missing(self, resource_name: str, consequence_value: float, demand_met: float, parameter_to_update: float) -> None:
-        self.patient_not_treated()
-        return parameter_to_update * max(consequence_value ** (self.demand[self.get_current_department_id()][resource_name] * (1-demand_met)), 1.0)
+        # self.patient_not_treated() # too conservative to assume that if a single nurse is missing, the patient is not treated, the patient is treated but with increased mortality rate/length of stay
+        nurse_demand = self.demand[self.get_current_department_id()][resource_name]
+        if nurse_demand > 1:
+            number_of_missing_nurses = nurse_demand * (1-demand_met)           
+        else:            
+            nurse_patient_ratio = 1/nurse_demand
+            number_of_missing_nurses = nurse_patient_ratio * (1-demand_met)
+        return parameter_to_update * max(consequence_value ** (number_of_missing_nurses), 1.0)
     
     def patient_not_treated(self) -> None:
         self.treated = False
@@ -166,6 +185,9 @@ class PatientType():
             self.flow.append({'Department': self.EXIT,
                         'TimeStepAtDepartment': [],
                         'TimeStepTreated': []})
+            
+    def record_mortality_rate(self) -> None:
+        self.mortality_rate_record.append(self.mortality_rate)
             
     def get_current_length_of_stay(self):
         return len(self.flow[-1]['TimeStepAtDepartment'])
